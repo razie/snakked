ca72b4759d7811848018af48666c7886ff8396a1
Razie <razie@razie.com>
2011-11-29 15:58:51 -0500

    upgrading to sbt 11

--- build.sbt
+++ build.sbt
@@ -1,19 +1,7 @@
+
 name := "snakked"
 
-organization := "com.razie"
-
-version := "0.1-SNAPSHOT"
-
-scalaVersion := "2.9.0-1"
+addSbtPlugin("net.databinder" % "posterous-sbt" % "0.3.2")
 
 retrieveManaged := true // copy libs in lib_managed
-
-publishTo <<= version { (v: String) =>
-  if(v endsWith "-SNAPSHOT")
-    Some(ScalaToolsSnapshots)
-  else
-    Some(ScalaToolsReleases)
-}
-
-credentials += Credentials((Path.userHome / ".ivy2.credentials").asFile) 
 
---
+++ project/Build.scala
@@ -0,0 +1,48 @@
+import sbt._
+import Keys._
+
+object V {
+  val version      = "0.3-SNAPSHOT"
+  val scalaVersion = "2.9.1"
+  val organization = "com.razie"
+
+  def snap = (if (V.version endsWith "-SNAPSHOT") "-SNAPSHOT" else "")
+
+  def RAZBASEVER = "0.5" + snap
+}
+
+object MyBuild extends Build {
+
+  def scalatest = "org.scalatest"  % "scalatest_2.9.1" % "1.6.1"
+  def junit     = "junit"          % "junit"           % "4.5"      % "test->default"
+  def json      = "org.json"       % "json"            % "20090211"
+  def jxpath    = "commons-jxpath" % "commons-jxpath"  % "1.3"
+  
+  def razBase = "com.razie" %% "razbase"         % V.RAZBASEVER
+
+
+  lazy val root = Project(id="snakked",    base=file("."),
+                          settings = defaultSettings ++ Seq()
+                  ) aggregate (core)
+
+  lazy val core = Project(id="snakk-core", base=file("core"),
+                          settings = defaultSettings ++ 
+                          Seq(libraryDependencies ++= Seq(scalatest, junit, json, jxpath, razBase))
+                  )
+
+  def defaultSettings = baseSettings ++ Seq()
+  def baseSettings = Defaults.defaultSettings ++ Seq (
+    scalaVersion := V.scalaVersion,
+    version      := V.version,
+    organization := V.organization,
+    organizationName := "Razie's Pub",
+    organizationHomepage := Some(url("http://www.razie.com")),
+    credentials += Credentials((Path.userHome / ".ivy2.credentials").asFile),
+    publishTo <<= version { (v: String) =>
+      if(v endsWith "-SNAPSHOT")
+        Some ("Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/")
+      else
+        Some ("Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/")
+    }  )
+
+}
