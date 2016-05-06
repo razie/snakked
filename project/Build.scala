3d06145ee1810eeef4ffe440c0afbf4eb1522945
Razvan Cojocaru <pub@razie.com>
2016-05-06 09:43:12 -0400

    updated to scala 2.11

--- project/Build.scala
+++ project/Build.scala
@@ -2,21 +2,23 @@ import sbt._
 import Keys._
 
 object V {
-  val version      = "0.6.7" //-SNAPSHOT"
-  val scalaVersion = "2.10.4" 
+  val version      = "0.9.1" //-SNAPSHOT"
+  val scalaVersion = "2.11.8" 
   val organization = "com.razie"
 
   def snap = (if (V.version endsWith "-SNAPSHOT") "-SNAPSHOT" else "")
 
-  def RAZBASEVER = "0.6.7" + snap
+  def RAZBASEVER = "0.9.1" + snap
 }
 
 object MyBuild extends Build {
 
-  def scalatest = "org.scalatest" %% "scalatest"       % "1.9.2"
+  def scalatest = "org.scalatest" %% "scalatest"       % "2.1.3"
   def junit     = "junit"          % "junit"           % "4.5"      % "test->default"
   def json      = "org.json"       % "json"            % "20090211"
   def jxpath    = "commons-jxpath" % "commons-jxpath"  % "1.3"
+
+  def scalaxml = "org.scala-lang.modules" %% "scala-xml" % "1.0.3"
   
   def razBase   = "com.razie"     %% "base"            % V.RAZBASEVER
 
@@ -27,7 +29,7 @@ object MyBuild extends Build {
 
   lazy val core = Project(id="snakk-core", base=file("core"),
                           settings = defaultSettings ++ 
-                          Seq(libraryDependencies ++= Seq(scalatest, junit, json, jxpath, razBase))
+                          Seq(libraryDependencies ++= Seq(scalatest, junit, json, jxpath, scalaxml, razBase))
                   )
 
   def defaultSettings = baseSettings ++ Seq()
@@ -38,6 +40,8 @@ object MyBuild extends Build {
     organization         := V.organization,
     organizationName     := "Razie's Pub",
     organizationHomepage := Some(url("http://www.razie.com")),
+    licenses := Seq("BSD-style" -> url("http://www.opensource.org/licenses/bsd-license.php")),
+    homepage := Some(url("http://www.razie.com")),
 
     publishTo <<= version { (v: String) =>
       if(v endsWith "-SNAPSHOT")
@@ -48,7 +52,7 @@ object MyBuild extends Build {
 
     resolvers ++= Seq(
       "snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
-      "releases"  at "https://oss.sonatype.org/content/repositories/releases") 
+      "releases"  at "https://oss.sonatype.org/content/repositories/public") 
     )
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,5 @@
 
 //addSbtPlugin("net.databinder" % "posterous-sbt" % "0.3.2")
 
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
+
